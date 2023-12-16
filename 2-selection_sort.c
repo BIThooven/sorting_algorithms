203068dc7a4f7c93babab1f10e3231c8d622f1c5
BIThooven <yassine.parkourist@gmail.com>
2023-12-16 14:34:49 +0100

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,21 +7,36 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, tmp;
+	size_t i, min_index, j;
 
-	if (*array == NULL || size < 2)
-		return;
 	for (i = 0; i < size - 1; i++)
 	{
+		min_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (array[j] < array[min_index])
 			{
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;
+				min_index = j;
 			}
 		}
-		print_array(array, size);
+		if (min_index != i)
+		{
+			swap(&array[i], &array[min_index]);
+			print_array(array, size);
+		}
 	}
+}
+/**
+ * swap - a function that swaps values in an array
+ * @a: a pointer to the value to be swapped
+ * @b: a pointer to the value to be swapped
+ * Return: void returns nothing
+*/
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
