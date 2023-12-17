f10bcd3fd874f3e4a56e923b449a6161184aad2c
BIThooven <yassine.parkourist@gmail.com>
2023-12-17 16:51:12 +0100

    shell sort

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void swap_nodes(listint_t **head, listint_t *node1, listint_t *node2);
 void selection_sort(int *array, size_t size);
 int lomuto(int *array, int low, int high);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
